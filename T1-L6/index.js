af2ad2242784a5281c728bd2e356945c9ae9f810
NguyetHa <nguyennguyetha0505@gmail.com>
2022-05-08 23:38:31 +0700

    NguyetHa C4EJS128

---
+++ T1-L6/index.js
@@ -0,0 +1,64 @@
+let tencuatoi="HaNN";
+console.log(HaNN);
+
+
+const MY_DOB="1998";
+const My_Name="HaNN";
+const Map="98 Hanoi";
+console.log(My_Name);
+console.log( Map);
+// // + - 
+--Boolean
+let boolean="3"==="3";
+--reverse 
+let boolean=!("3"==="3");
+let notboolean= false;
+
+let boolean="chung" = "CHUNG".toLowerCase
+let boolean= true;
+
+let A=true;
+let B=true;
+let C=false;
+
+let boolean= ((A||B) && C) || (B && (A || C));
+
+
+let A= '';
+let boolean = !!A;
+// !!
+// false ""
+console.log(boolean);
+
+--CSSConditionRule
+
+if(1 >2) {
+    console.log("Sai")
+}
+
+
+--String
+let myStr = "123";
+console.log(+myStr);
+console.log(typeof +myStr)
+let monthiStr = "5"
+let monthNum = 5
+
+
+
+--Object
+
+--switch{
+case
+console.log
+break
+default
+}
+
+
+
+
+
+
+
+
